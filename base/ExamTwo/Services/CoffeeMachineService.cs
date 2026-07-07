f3e6af635097cf972418fb9671366435ff08f888
Your Name <you@example.com>
2026-07-06 19:05:08 -0600

    Se eliminan DTos de service al haberlo cambiado de carpeta

--- base/ExamTwo/Services/CoffeeMachineService.cs
+++ base/ExamTwo/Services/CoffeeMachineService.cs
@@ -13,16 +13,4 @@ namespace ExamTwo.Services
         }
     }
 
-    public class OrderRequest
-    {
-        public Dictionary<string, int> Order { get; set; }
-        public Payment Payment { get; set; }
-    }
-
-    public class Payment
-    {
-        public int TotalAmount { get; set; }
-        public List<int> Coins { get; set; }
-        public List<int> Bills { get; set; }
-    }
 }
