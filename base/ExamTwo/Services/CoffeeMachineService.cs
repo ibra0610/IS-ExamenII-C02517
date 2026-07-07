036c04aec24ec56aa08d4ff023732378682a2906
Your Name <you@example.com>
2026-07-06 18:57:08 -0600

    Se crea service base para la maquina de cafe

---
+++ base/ExamTwo/Services/CoffeeMachineService.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace ExamTwo.Services
+{
+    public class CoffeeMachineService : Service
+    {
+
+        private readonly Database _db;
+
+        public CoffeeMachineService(Database db)
+        {
+            _db = db;
+        }
+    }
+
+    public class OrderRequest
+    {
+        public Dictionary<string, int> Order { get; set; }
+        public Payment Payment { get; set; }
+    }
+
+    public class Payment
+    {
+        public int TotalAmount { get; set; }
+        public List<int> Coins { get; set; }
+        public List<int> Bills { get; set; }
+    }
+}
