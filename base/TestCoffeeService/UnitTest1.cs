4992e3860bf47e84f728a24be8ca21ab6053e5cb
Your Name <you@example.com>
2026-07-06 20:57:25 -0600

    Inicio de unit testing

--- base/TestCoffeeService/UnitTest1.cs
+++ base/TestCoffeeService/UnitTest1.cs
@@ -1,10 +1,25 @@
-﻿namespace TestCoffeeService
+﻿using ExamTwo.Services;
+using ExamTwo.Repositories;
+
+namespace TestCoffeeService
 {
     public class UnitTest1
     {
+        private readonly Database _db;
+        private readonly _CoffeeMachineService new CoffeeMachineService(_db);
         [Fact]
-        public void Test1()
+        public void GetCoffeeInventory()
         {
+            //Arrange
+
+
+
+            //Act
+
+
+
+            //Assert
+
 
         }
     }
